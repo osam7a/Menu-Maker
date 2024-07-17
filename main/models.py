cd3f7d1af31813448a5716bd808e7c9691b067e3
Osama Alhennawi <osamaziadalhinawi@gmail.com>
2024-07-18 00:31:01 +0400

    Added template selection & resturant name fields in Menu

--- main/models.py
+++ main/models.py
@@ -3,12 +3,14 @@ from django.db import models
 # Create your models here.
 class Menu(models.Model):
     title = models.CharField(max_length=100)
-    logo = models.ImageField(upload_to='images/')
+    resturaunt_name = models.CharField(default="Resturaunt", max_length=100)
+    logo = models.ImageField(upload_to='static/uploads/')
     address = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
     maps_link = models.CharField(blank=True, max_length=100)
 
     currency = models.CharField(max_length=10, default='EUR')
+    template = models.CharField(max_length=100, default='basic')
 
     def __str__(self):
         return self.title
