9eb9e8d01c64b65832c198652a648983100f6c61
Osama Alhennawi <osamaziadalhinawi@gmail.com>
2024-07-21 01:34:51 +0400

    add the featured label into model translation

--- main/translation.py
+++ main/translation.py
@@ -4,15 +4,12 @@ from main.models import Item, Category, Menu
 
 @register(Item)
 class ItemTranslationOptions(TranslationOptions):
-    fields = ('title', 'ingredients')
-    required_languages = ('en',)
+    fields = ('title', 'ingredients', 'featured_label')
 
 @register(Category)
 class CategoryTranslationOptions(TranslationOptions):
     fields = ('title',)
-    required_languages = ('en',)
 
 @register(Menu)
 class MenuTranslationOptions(TranslationOptions):
     fields = ('title', 'address')
-    required_languages = ('en',)
